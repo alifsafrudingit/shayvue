705a4bbff499af80061ad33ebdb19fd1c1b09939
alifsafrudingit <alifsafrudin94@gmail.com>
2021-03-17 21:46:15 +0700

    Update cart

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -26,42 +26,25 @@
                 <div class="product-pic-zoom">
                   <img class="product-big-img" :src="default_image" alt="" />
                 </div>
-                <div class="product-thumbs">
+                <div
+                  class="product-thumbs"
+                  v-if="productDetails.galleries.length > 0"
+                >
                   <carousel
                     :dots="false"
                     :nav="false"
                     class="product-thumbs-track ps-slider"
                   >
                     <div
+                      v-for="miniPicture in productDetails.galleries"
+                      :key="miniPicture.id"
                       class="pt"
-                      @click="changeImage(thumbs[0])"
-                      :class="thumbs[0] == default_image ? 'active' : ''"
+                      @click="changeImage(miniPicture.photo)"
+                      :class="
+                        miniPicture.photo == default_image ? 'active' : ''
+                      "
                     >
-                      <img src="img/mickey1.jpg" alt="" />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(thumbs[1])"
-                      :class="thumbs[1] == default_image ? 'active' : ''"
-                    >
-                      <img src="img/mickey2.jpg" alt="" />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(thumbs[2])"
-                      :class="thumbs[2] == default_image ? 'active' : ''"
-                    >
-                      <img src="img/mickey3.jpg" alt="" />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(thumbs[3])"
-                      :class="thumbs[3] == default_image ? 'active' : ''"
-                    >
-                      <img src="img/mickey4.jpg" alt="" />
+                      <img :src="miniPicture.photo" alt="" />
                     </div>
                   </carousel>
                 </div>
@@ -69,40 +52,29 @@
               <div class="col-lg-6">
                 <div class="product-details text-left">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{ productDetails.type }}</span>
+                    <h3>{{ productDetails.name }}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Corporis, error officia. Rem aperiam laborum voluptatum
-                      vel, pariatur modi hic provident eum iure natus quos non a
-                      sequi, id accusantium! Autem.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Quam possimus quisquam animi, commodi, nihil voluptate
-                      nostrum neque architecto illo officiis doloremque et
-                      corrupti cupiditate voluptatibus error illum. Commodi
-                      expedita animi nulla aspernatur. Id asperiores blanditiis,
-                      omnis repudiandae iste inventore cum, quam sint molestiae
-                      accusamus voluptates ex tempora illum sit perspiciatis.
-                      Nostrum dolor tenetur amet, illo natus magni veniam quia
-                      sit nihil dolores. Commodi ratione distinctio harum
-                      voluptatum velit facilis voluptas animi non laudantium, id
-                      dolorem atque perferendis enim ducimus? A exercitationem
-                      recusandae aliquam quod. Itaque inventore obcaecati, unde
-                      quam impedit praesentium veritatis quis beatae ea atque
-                      perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00</h4>
+                    <p v-html="productDetails.description"></p>
+                    <h4>${{ productDetails.price }}</h4>
                   </div>
                   <div class="quantity">
-                    <router-link to="/cart"
-                      ><a href="#" class="primary-btn pd-cart"
+                    <router-link to="/cart">
+                      <a
+                        @click="
+                          saveCart(
+                            productDetails.id,
+                            productDetails.name,
+                            productDetails.price,
+                            productDetails.galleries[0].photo
+                          )
+                        "
+                        href="#"
+                        class="primary-btn pd-cart"
                         >Add To Cart</a
-                      ></router-link
-                    >
+                      >
+                    </router-link>
                   </div>
                 </div>
               </div>
@@ -125,6 +97,8 @@ import Related from "@/components/Related.vue";
 import Footer from "@/components/Footer.vue";
 import carousel from "vue-owl-carousel";
 
+import axios from "axios";
+
 export default {
   name: "product",
   components: {
@@ -135,19 +109,50 @@ export default {
   },
   data() {
     return {
-      default_image: "img/mickey1.jpg",
-      thumbs: [
-        "img/mickey1.jpg",
-        "img/mickey2.jpg",
-        "img/mickey3.jpg",
-        "img/mickey4.jpg"
-      ]
+      default_image: "",
+      productDetails: [],
+      cartUser: []
     };
   },
   methods: {
     changeImage(urlImage) {
       this.default_image = urlImage;
+    },
+    setDataPicture(data) {
+      // replace objeect productDeails dengan data dari API
+      this.productDetails = data;
+      //  replace value gambar default dengan data dari API (galleries)
+      this.default_image = data.galleries[0].photo;
+    },
+    saveCart(idProduct, nameProduct, priceProduct, photoProduct) {
+      let productStored = {
+        id: idProduct,
+        name: nameProduct,
+        price: priceProduct,
+        photo: photoProduct
+      };
+      this.cartUser.push(productStored);
+      const parsed = JSON.stringify(this.cartUser);
+      localStorage.setItem("cartUser", parsed);
     }
+  },
+  mounted() {
+    if (localStorage.getItem("cartUser")) {
+      try {
+        this.cartUser = JSON.parse(localStorage.getItem("cartUser"));
+      } catch (e) {
+        localStorage.removeItem("cartUser");
+      }
+    }
+    axios
+      .get("http://shayna-backend.belajarkoding.com/api/products", {
+        params: {
+          id: this.$route.params.id
+        }
+      })
+      .then(res => this.setDataPicture(res.data.data))
+      // eslint-disable-next-lone no-console
+      .catch(err => console.log(err));
   }
 };
 </script>
--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -34,31 +34,18 @@
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
+                      <tr v-for="cart in cartUser" :key="cart.id">
                         <td class="cart-pic first-row">
-                          <img src="img/cart-page/product-1.jpg" />
+                          <img class="img-cart" :src="cart.photo" />
                         </td>
                         <td class="cart-title first-row text-center">
-                          <h5>Pure Pineapple</h5>
+                          <h5>{{ cart.name }}</h5>
                         </td>
-                        <td class="p-price first-row">$60.00</td>
-                        <td class="delete-item">
-                          <a href="#"
-                            ><i class="material-icons">
-                              close
-                            </i></a
-                          >
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="cart-pic first-row">
-                          <img src="img/cart-page/product-1.jpg" />
-                        </td>
-                        <td class="cart-title first-row text-center">
-                          <h5>Pure Pineapple</h5>
-                        </td>
-                        <td class="p-price first-row">$60.00</td>
-                        <td class="delete-item">
+                        <td class="p-price first-row">${{ cart.price }}</td>
+                        <td
+                          @click="removeItem(cartUser.index)"
+                          class="delete-item"
+                        >
                           <a href="#"
                             ><i class="material-icons">
                               close
@@ -84,6 +71,7 @@
                         id="namaLengkap"
                         aria-describedby="namaHelp"
                         placeholder="Masukan Nama"
+                        v-model="customerInfo.name"
                       />
                     </div>
                     <div class="form-group">
@@ -94,6 +82,7 @@
                         id="emailAddress"
                         aria-describedby="emailHelp"
                         placeholder="Masukan Email"
+                        v-model="customerInfo.email"
                       />
                     </div>
                     <div class="form-group">
@@ -104,6 +93,7 @@
                         id="noHP"
                         aria-describedby="noHPHelp"
                         placeholder="Masukan No. HP"
+                        v-model="customerInfo.number"
                       />
                     </div>
                     <div class="form-group">
@@ -112,6 +102,7 @@
                         class="form-control"
                         id="alamatLengkap"
                         rows="3"
+                        v-model="customerInfo.address"
                       ></textarea>
                     </div>
                   </form>
@@ -127,10 +118,15 @@
                     <li class="subtotal">
                       ID Transaction <span>#SH12000</span>
                     </li>
-                    <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
-                    <li class="subtotal mt-3">Pajak <span>10%</span></li>
                     <li class="subtotal mt-3">
-                      Total Biaya <span>$440.00</span>
+                      Subtotal <span>${{ totalPrice }}</span>
+                    </li>
+                    <li class="subtotal mt-3">
+                      Pajak <span>10% ${{ countCharge }}</span>
+                    </li>
+                    <li class="subtotal mt-3">
+                      Total Biaya
+                      <span>${{ finalPrice }}</span>
                     </li>
                     <li class="subtotal mt-3">
                       Bank Transfer <span>Mandiri</span>
@@ -139,14 +135,14 @@
                       No. Rekening <span>2208 1996 1403</span>
                     </li>
                     <li class="subtotal mt-3">
-                      Nama Penerima <span>Shayna</span>
+                      Nama Penerima <span>Alif Safrudin</span>
                     </li>
                   </ul>
-                  <router-link to="/success"
-                    ><a href="#" class="proceed-btn"
-                      >I ALREADY PAID</a
-                    ></router-link
+                  <!-- <router-link to="/success"> -->
+                  <a @click="checkout()" href="#" class="proceed-btn"
+                    >I ALREADY PAID</a
                   >
+                  <!-- </router-link> -->
                 </div>
               </div>
             </div>
@@ -160,11 +156,83 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import axios from "axios";
 
 export default {
   name: "cart",
   components: {
     Header
+  },
+  data() {
+    return {
+      cartUser: [],
+      customerInfo: {
+        name: "",
+        email: "",
+        number: "",
+        address: ""
+      }
+    };
+  },
+  methods: {
+    removeItem(index) {
+      this.cartUser.splice(index, 1);
+      const parsed = JSON.stringify(this.cartUser);
+      localStorage.setItem("cartUser", parsed);
+    },
+    // function for sent data to API
+    checkout() {
+      let productIds = this.cartUser.map(function(product) {
+        return product.id;
+      });
+
+      let checkoutData = {
+        name: this.customerInfo.name,
+        email: this.customerInfo.email,
+        number: this.customerInfo.number,
+        address: this.customerInfo.address,
+        transaction_total: this.finalPrice,
+        transaction_status: "PENDING",
+        transaction_details: productIds
+      };
+
+      axios
+        .post(
+          "http://shayna-backend.belajarkoding.com/api/checkout",
+          checkoutData
+        )
+        .then(() => this.$router.push("success"))
+        .catch(err => console.log(err));
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("cartUser")) {
+      try {
+        this.cartUser = JSON.parse(localStorage.getItem("cartUser"));
+      } catch (e) {
+        localStorage.removeItem("cartUser");
+      }
+    }
+  },
+  computed: {
+    totalPrice() {
+      return this.cartUser.reduce(function(items, data) {
+        return items + data.price;
+      }, 0);
+    },
+    countCharge() {
+      return (this.totalPrice * 10) / 100;
+    },
+    finalPrice() {
+      return this.totalPrice + this.countCharge;
+    }
   }
 };
 </script>
+
+<style scoped>
+.img-cart {
+  width: 100px;
+  height: 100px;
+}
+</style>
